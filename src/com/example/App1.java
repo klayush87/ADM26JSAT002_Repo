ac5737391853aed9aa15f810cce3527183f9913e
2507433 <2507433@LTIN709866.cts.com>
2026-05-13 12:46:29 +0530

    First Commit

---
+++ src/com/example/App1.java
@@ -0,0 +1,30 @@
+package com.example;
+import java.util.*;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Ayush in java world");
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Customer id  ");
+		
+		int cid= sc.nextInt();
+		
+		System.out.println("Enter the Name  ");
+		
+		String name=sc.next();
+		
+		System.out.println("Enter the Sallary  ");
+		
+		double salary=sc.nextDouble();
+		
+		System.out.println("Enter the Name "+cid);
+		System.out.println("Enter the Name "+name);
+		System.out.println("Enter the Name "+salary);
+		
+
+	}
+
+}
